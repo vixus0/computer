094a339e69710b08b061432137cdde5828a5cdbb
Anshul Sirur <anshul@vixus0.dev>
2024-01-02 12:57:42 +0100

    nvim initial configuration

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+-- config
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.background = 'dark'
+vim.opt.wrap = false
+vim.opt.list = true
+vim.opt.swapfile = false
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.path:append('**')
+vim.opt.termguicolors = true
